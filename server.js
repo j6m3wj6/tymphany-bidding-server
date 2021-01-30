6bbd812f11e8de865f6ff1cbe8f20503ab6c1084
Tong <b05502072@ntu.edu.tw>
2021-01-31 02:24:43 +0800

    add cors

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 const app = express();
+const corsOptions = {
+  origin: ['https://j6m3wj6.github.io/react-gh-pages-demo/', 'http://localhost:3000','http://127.0.0.1:3000'],
+  credentials: true,
+}
 
 process.env.MONGO_URL = "mongodb+srv://j6m3wj6:0000@cluster0.gnphr.mongodb.net/Tymphany-Bidding?retryWrites=true&w=majority";
 const uri = process.env.MONGODB_URI;
 
 // use the express-static middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, DELETE, GET");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors(corsOptions))
 app.use(express.static("public"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
