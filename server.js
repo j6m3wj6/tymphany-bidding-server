41e3afc17029b19ea44f6e5e3966be3939a4839a
Tong <b05502072@ntu.edu.tw>
2021-02-01 10:58:20 +0800

    list bidding price

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors(corsOptions))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, DELETE, GET");
+  res.header("Access-Control-Allow-Methods", "PUT, DELETE, GET, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
